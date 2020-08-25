7d3883cd04cc3df9f2b99a9eeff738db7a44d239
canoe <canoe955yan@gmail.com>
2020-08-26 00:33:16 +0800

    提交Git这个项目到本地仓库去

---
+++ src/com/git/reponsitory/Demo.java
@@ -0,0 +1,14 @@
+package com.git.reponsitory;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello world !!! ");
+        System.out.println("Hello world !!! ");
+        System.out.println("Hello world !!! ");
+        System.out.println("Hello world !!! ");
+        System.out.println("Hello world !!! ");
+        System.out.println("Hello world !!! ");
+        System.out.println("Hello world !!! ");
+
+    }
+}
