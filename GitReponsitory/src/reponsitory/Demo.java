e28cd95564cd98f99bab5d567817df91d019f680
canoe <canoe955yan@gmail.com>
2020-08-26 00:55:54 +0800

    在克隆下来的项目中 修改test()方法体的内容

--- GitReponsitory/src/reponsitory/Demo.java
+++ GitReponsitory/src/reponsitory/Demo.java
@@ -14,5 +14,12 @@ public class Demo {
 
     public void test(){
         System.out.println("Hello Git");
+        // 在克隆下来的项目中 修改test()方法体的内容
+        int i = 520 ;
+        int j = 1314 ;
+        if (i != j ){
+            System.out.println(" I love you  no equal to one's whole life !");
+        }
+
     }
 }
